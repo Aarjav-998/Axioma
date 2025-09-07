a8ab9ccb2d31e80274f33e3636fba71fcca2479b
Websi <web.sidehustle2025@gmail.com>
2025-09-07 12:37:30 +0530

    ready for announcement

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,33 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Axioma || Elite Opportunities for High School Leaders",
-  description: "Axioma provides high school students with impactful, real-world opportunities in diverse fields like entrepreneurship, policy, tech, and research. Through fellowships, competitions, and mentorship, we empower future leaders to make an impact now.",
+    title: "Axioma || Elite Opportunities for High School Leaders",
+    description:
+        "Axioma empowers ambitious high school leaders with elite opportunities in innovation, policy, entrepreneurship, and global impact. Build your future with mentorship, events, and a world-class network.",
+    keywords: [
+        "Axioma",
+        "high school leaders",
+        "student leadership",
+        "elite opportunities",
+        "innovation challenges",
+        "policy",
+        "entrepreneurship",
+        "mentorship",
+        "youth leadership",
+        "future leaders",
+    ],
+    openGraph: {
+        type: "website",
+        url: "https://axioma.co.in/",
+        title: "Axioma | Elite Opportunities for High School Leaders",
+        description:
+            "Empowering ambitious high school leaders through innovation, policy, entrepreneurship, and global opportunities.",
+
+    },
+
+    icons: {
+        icon: "/favicon.ico",
+    },
 };
 
 export default function RootLayout({
@@ -29,7 +54,7 @@ export default function RootLayout({
 
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-20`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-20 `}
 
       >
       <Navbar/>
